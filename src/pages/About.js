245efa56685a2ec9fc5f2689d9ef5d71a6c945d7
Bora <bora.karaca@ogr.iu.edu.tr>
2022-05-28 15:50:23 +0300

    completed application

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui
+        neque, eum fuga impedit expedita dolore maiores obcaecati! Minus
+        incidunt facere voluptates cum illum architecto, perspiciatis eligendi
+        temporibus aperiam sed suscipit, facilis tempore id reprehenderit!
+        Laboriosam fugit dolorem velit aspernatur?
+      </p>
+    </section>
+  );
+};
+
+export default About;
